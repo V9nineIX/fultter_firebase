34025ea57409d77afe4c76eaef0add25844f91a2
Wi9 <v9nineix@gmail.com>
2019-09-16 14:30:12 +0700

    re factor code /

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(MyApp());
 
-//class MyApp extends StatelessWidget {
-//  // This widget is the root of your application.
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'Flutter Demo',
-//      theme: ThemeData(
-//        primarySwatch: Colors.blue,
-//      ),
-//      home: Container()
-//    );
-//  }
-//}
+
 
 final dummySnapshot = [
   {"name": "Filip", "votes": 15},
